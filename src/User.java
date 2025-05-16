60d65244661abaab700b6e13fbcc41f86d51f738
idnkendy <idnkendy@gmail.com>
2025-05-16 10:01:32 +0700

    Getter and Getter User.java
    Adding getter and setter

--- src/User.java
+++ src/User.java
@@ -81,4 +81,36 @@ public User(String userId, String userName, String userPassword,
             + "}";
         }
 
+    //getter and setter
+    public String getUserId() {
+        return userId;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public String getUserPassword() {
+        return userPassword;
+    }
+    public String getUserRegisterTime() {
+        return userRegisterTime;
+    }
+    public String getUserRole() {
+        return userRole;
+    }
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+    public void setUserRegisterTime(String userRegisterTime) {
+        this.userRegisterTime = userRegisterTime;
+    }
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+
 }
