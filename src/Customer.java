4f5cfc8d6ea6a6e21848efc0d1a48812c7c212a4
idnkendy <idnkendy@gmail.com>
2025-05-16 10:17:22 +0700

    Adding some method Customer.java
    adding getter and setter, check constructor

--- src/Customer.java
+++ src/Customer.java
@@ -2,27 +2,53 @@ public class Customer extends User {
     private String userEmail;
     private String userMobile;
     
-    public Customer(String userId, String userName, String userPassword,
- String userRegisterTime, String userRole,
- String userEmail, String userMobile) {
+    public Customer(String userId, String userName, String userPassword,String userRegisterTime, String userRole,String userEmail, String userMobile) {
  
     super( userId,userName,userPassword,userRegisterTime,userRole);
-    this.userEmail=userEmail;
     this.userMobile=userMobile;
+
+    //Check email
+    if (isValidEmail(userEmail)==true)
+    this.userEmail=userEmail;
+    else {
+      System.out.println("Invalid email, auto set to default email example@gmail.com");
+      this.userEmail="example@gmail.com";
+    }
    
 }
- // default constructor
+   // Default constructor
    public Customer(){
-         super();
+
    }
+   //Getter and Setter
+   public String getUserEmail() {
+       return userEmail;
+   }
+   public String getUserMobile() {
+       return userMobile;
+   }
+   public void setUserEmail(String userEmail) {
+       this.userEmail = userEmail;
+   }
+   public void setUserMobile(String userMobile) {
+       this.userMobile = userMobile;
+   }
+
+   private boolean isValidEmail(String email) {
+        if (email == null) return false;
+        // Regex để kiểm tra định dạng email
+        String emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+        return email.matches(emailRegex);
+    }
+
    @Override
    public String toString(){
       return "{"
-        + "\"user_id\": \"" + userId + "\", "
-        + "\"user_name\": \"" + userName + "\", "
-        + "\"user_password\": \"" + userPassword + "\", "
-        + "\"user_register_time\": \"" + userRegisterTime + "\", "
-        + "\"user_role\": \"" + userRole + "\""
+        + "\"user_id\": \"" + getUserId() + "\", "
+        + "\"user_name\": \"" + getUserName() + "\", "
+        + "\"user_password\": \"" + getUserPassword() + "\", "
+        + "\"user_register_time\": \"" + getUserRegisterTime() + "\", "
+        + "\"user_role\": \"" + getUserRole() + "\""
         + "\"user_email\": \" "+ userEmail+"@gmail.com \", "
         + "\"user_mobile\":\""+ userMobile
         + "}";
