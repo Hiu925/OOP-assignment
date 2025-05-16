bf8683483b200b5a13ebc8501b2ffc1c75b90e54
idnkendy <idnkendy@gmail.com>
2025-05-17 01:26:06 +0700

    Update Constructor Admin.java

--- src/Admin.java
+++ src/Admin.java
@@ -1,31 +1,29 @@
 public class Admin extends User {
     
-   public Admin(String userId, String userName, String userPassword,
- String userRegisterTime, String userRole) {
-    
-    super(userId,userName,userPassword,userRegisterTime);
- 
-     super.userRole = (userRole == null || userRole.isBlank()) ? "admin" : userRole;
-
+   public Admin(String userId, String userName, String userPassword,String userRegisterTime, String userRole) {
+   super(userId,userName,userPassword,userRegisterTime);
+   if (userRole == null || userRole.isEmpty() || !userRole.equals("admin")){
+       System.out.printf("Invalid userRole of user %s, auto set default into admin",userId);
+       super.setUserRole("admin");
    }
+   super.setUserRole("admin");
+}
 
-/**
-* Default constructor
-*/
-public Admin() {
- // Implementation with default values
+   //Default constructor
+   public Admin() {
+   // Implementation with default values
    }
 
    @Override
-public String toString() {
- return "{" 
-        + "\"user_id\": \"" + userId + "\", "
-        + "\"user_name\": \"" + userName + "\", "
-        + "\"user_password\": \"" + userPassword + "\", "
-        + "\"user_register_time\": \"" + userRegisterTime + "\", "
-        + "\"user_role\": \"" + userRole + "\""
+   public String toString() {
+   return "{" 
+        + "\"user_id\": \"" + getUserId() + "\", "
+        + "\"user_name\": \"" + getUserName() + "\", "
+        + "\"user_password\": \"" + getUserPassword() + "\", "
+        + "\"user_register_time\": \"" + getUserRegisterTime() + "\", "
+        + "\"user_role\": \"" + getUserRole() + "\""
         + "}";
-}
+   }
 
 
 }
