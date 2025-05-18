ab8ec4c8d78187678bea1d8f81005310d3f472bf
Hiu925 <nguyenhieu072005@gmail.com>
2025-05-18 15:24:43 +0700

    Initial commit

--- src/ProductOperation.java
+++ src/ProductOperation.java
@@ -1,3 +1,15 @@
 public class ProductOperation {
     
+    private static ProductOperation instance;
+
+    
+    private ProductOperation() {
+      
+    }
+    public static ProductOperation getInstance() {
+        if (instance == null) {
+            instance = new ProductOperation();
+        }
+        return instance;
+    }
 }
